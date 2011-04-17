42352336d549a69c0ef12d1824776d09cac14b37
Dave Cochran <dave.l.cochran@gmail.com>
2011-04-17 08:39:07 -0400

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -18,7 +18,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
-                        :content => @base_title + " | Home")
+                        :content => "Home")
     end
   end
 
@@ -31,7 +31,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title", 
-                        :content => @base_title + " | Contact")
+                        :content => "Contact")
     end    
   end
 
@@ -44,7 +44,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title", 
-                        :content => @base_title + " | About")
+                        :content => "About")
     end        
   end
   
@@ -57,7 +57,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title", 
-                        :content => @base_title + " | Help")
+                        :content => "Help")
     end        
   end
    
